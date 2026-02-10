c31f34e1f62043adfdbf8d761cfcb7bf00354535
pollomax847 <bain.paul24@gmail.com>
2026-02-10 21:35:40 +0100

    Ajustement Android build config
    - Suppression ndkVersion pour éviter conflits de licence
    - targetSdk fixé à 33 pour compatibilité
    - Configuration prête pour build APK (nécessite licences Android acceptées)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
 android {
     namespace = "com.example.site_m_mo_vmc"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -25,7 +24,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 33
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
